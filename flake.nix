b85fc5ca5d62a120a8eb7964c9fe1330f1ac0047
Michael Strobel <mstrobel97@gmail.com>
2025-03-11 08:07:29 +0100

    chore: update nix files

--- flake.nix
+++ flake.nix
@@ -6,37 +6,43 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }: let
-    plugin-overlay = final: prev: {
-      session-nvim = final.pkgs.vimUtils.buildVimPlugin {
-        name = "session.nvim";
-        src = self;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      plugin-overlay = final: prev: {
+        session-nvim = final.pkgs.vimUtils.buildVimPlugin {
+          name = "session.nvim";
+          src = self;
+        };
       };
-    };
 
-    supportedSystems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-  in
-    flake-utils.lib.eachSystem supportedSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          plugin-overlay
-        ];
-      };
-    in {
-      packages = rec {
-        default = session-nvim;
-        inherit (pkgs) session-nvim;
-      };
-    })
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            plugin-overlay
+          ];
+        };
+      in
+      {
+        packages = rec {
+          default = session-nvim;
+          inherit (pkgs) session-nvim;
+        };
+      }
+    )
     // {
       overlays.default = plugin-overlay;
     };
